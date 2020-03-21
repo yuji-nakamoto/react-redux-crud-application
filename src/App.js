b96a2a87c7be3687d28603e5c83d793f7ec432c1
yuji-nakamoto <yuji0411@icloud.com>
2020-03-22 07:45:03 +0900

    leran jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div><h1>Hello, world!</h1></div>;
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked")}} />
+      </React.Fragment>
+    )
   }
 }
 
