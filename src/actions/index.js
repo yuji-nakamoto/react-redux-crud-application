ba2c949cba7884e6aa516fdb3e403016dd1b2a37
yuji-nakamoto <yuji0411@icloud.com>
2020-03-22 22:41:52 +0900

    implement delete feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
 export const CREATE_EVENT = 'CREATE_EVENT'
+export const DELETE_EVENT = 'DELETE_EVENT'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -18,3 +19,7 @@ export const postEvent = values => async dispatch => {
   dispatch({ type: CREATE_EVENT, response })
 }
 
+export const deleteEvent = id => async dispatch => {
+  await axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+  dispatch({ type: DELETE_EVENT, id })
+}
